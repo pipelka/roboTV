2697dd1ceaee2ebf8499a4d18a29cc2448af50cb
Alexander Pipelka <alexander.pipelka@gmail.com>
2016-02-08 23:10:03 +0100

    [SpinnerFragment] added dark background

--- app/src/main/java/org/xvdr/recordings/fragment/SpinnerFragment.java
+++ app/src/main/java/org/xvdr/recordings/fragment/SpinnerFragment.java
@@ -8,23 +8,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.xvdr.robotv.R;
 
 public class SpinnerFragment extends Fragment {
 
     private static final String TAG = SpinnerFragment.class.getSimpleName();
 
-    private static final int SPINNER_WIDTH = 100;
-    private static final int SPINNER_HEIGHT = 100;
+    private static final int SPINNER_WIDTH = 200;
+    private static final int SPINNER_HEIGHT = 200;
+
+    protected ProgressBar mProgressBar;
+    protected TextView mText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ProgressBar progressBar = new ProgressBar(container.getContext());
+        View view = inflater.inflate(R.layout.fragment_spinner, container, false);
 
-        if (container instanceof FrameLayout) {
-            FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(SPINNER_WIDTH, SPINNER_HEIGHT, Gravity.CENTER);
-            progressBar.setLayoutParams(layoutParams);
-        }
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(SPINNER_WIDTH, SPINNER_HEIGHT, Gravity.CENTER);
+        mProgressBar.setLayoutParams(layoutParams);
 
-        return progressBar;
+        mText = (TextView) view.findViewById(R.id.text);
+
+        return view;
     }
 }
