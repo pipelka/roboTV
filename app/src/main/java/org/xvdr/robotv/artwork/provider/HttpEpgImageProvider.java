51140aac49e76f6270e8053f41e45157fc878865
Alexander Pipelka <alexander.pipelka@gmail.com>
2016-02-14 13:00:51 +0100

    [artwork] added HttpEpgImageProvider
    this class takes a template url and transforms it with help of the event id:
    
    http://www.x.com/data/%d.jpg -> http://www.x.com/data/12345.jpg
    
    The resulting url defines the path to the artwork if it exists

---
+++ app/src/main/java/org/xvdr/robotv/artwork/provider/HttpEpgImageProvider.java
@@ -0,0 +1,37 @@
+package org.xvdr.robotv.artwork.provider;
+
+import org.xvdr.robotv.artwork.ArtworkHolder;
+import org.xvdr.robotv.artwork.Event;
+
+import java.io.IOException;
+
+public class HttpEpgImageProvider extends SimpleArtworkProvider {
+
+    private String mTemplateUrl;
+
+    public HttpEpgImageProvider(String templateUrl) {
+        mTemplateUrl = templateUrl;
+    }
+
+    @Override
+    public ArtworkHolder search(Event event) throws IOException {
+        if(mTemplateUrl.isEmpty()) {
+            return null;
+        }
+
+        String url;
+
+        try {
+            url = String.format(mTemplateUrl, event.getEventId());
+        }
+        catch(java.util.IllegalFormatException e) {
+            return null;
+        }
+
+        if(!checkUrlExists(url)) {
+            return null;
+        }
+
+        return new ArtworkHolder("", url);
+    }
+}
