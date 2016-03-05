4fa8329ce427420172ce0f5caf53afe74a05928a
tunip <dunkel@posteo.de>
2016-03-05 11:24:08 +0100

    Add EPISODE_TITLE to the program guide

--- app/src/main/java/org/xvdr/sync/ChannelSyncAdapter.java
+++ app/src/main/java/org/xvdr/sync/ChannelSyncAdapter.java
@@ -441,20 +441,13 @@ public class ChannelSyncAdapter {
             String posterUrl = resp.getString();
             String backgroundUrl = resp.getString();
 
-            String description = plotOutline.trim();
-
-            if(!description.isEmpty() && !plot.isEmpty()) {
-                description += " - ";
-            }
-
-            description += plot;
-
             Event event = new Event(content, title, plotOutline, plot, eventDuration, eventId);
 
             ContentValues values = new ContentValues();
             values.put(TvContract.Programs.COLUMN_CHANNEL_ID, channelId);
             values.put(TvContract.Programs.COLUMN_TITLE, event.getTitle());
-            values.put(TvContract.Programs.COLUMN_SHORT_DESCRIPTION, description.substring(0, Math.min(description.length(), 400)));
+            values.put(TvContract.Programs.COLUMN_EPISODE_TITLE, plotOutline);
+            values.put(TvContract.Programs.COLUMN_SHORT_DESCRIPTION, plot);
             values.put(TvContract.Programs.COLUMN_LONG_DESCRIPTION, event.getPlot());
             values.put(TvContract.Programs.COLUMN_START_TIME_UTC_MILLIS, startTime * 1000);
             values.put(TvContract.Programs.COLUMN_END_TIME_UTC_MILLIS, endTime * 1000);
