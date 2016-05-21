d680388c7690b1ee8976f921dee926cd2ccfdcf7
Alexander Pipelka <alexander.pipelka@gmail.com>
2016-05-21 08:28:25 +0200

    [PacketQueue] use non-blocking ArrayDeque

--- app/src/main/java/org/xvdr/extractor/PacketQueue.java
+++ app/src/main/java/org/xvdr/extractor/PacketQueue.java
@@ -1,12 +1,10 @@
 package org.xvdr.extractor;
 
-import android.support.v4.util.Pools;
-
 import com.google.android.exoplayer.MediaFormat;
 
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.ArrayDeque;
 
-public class PacketQueue extends ArrayBlockingQueue<Allocation> {
+public class PacketQueue extends ArrayDeque<Allocation> {
 
     final private static String TAG = "PacketQueue";
     private int mQueueSize = 400;
@@ -22,21 +20,11 @@ public class PacketQueue extends ArrayBlockingQueue<Allocation> {
         Allocation holder = new Allocation(format);
         mFormat = format;
 
-        try {
-            put(holder);
-        }
-        catch(InterruptedException e) {
-            e.printStackTrace();
-        }
+        add(holder);
     }
 
     synchronized public void sampleData(Allocation buffer) {
-        try {
-            put(buffer);
-        }
-        catch(InterruptedException e) {
-            e.printStackTrace();
-        }
+        add(buffer);
     }
 
     public MediaFormat getFormat() {
