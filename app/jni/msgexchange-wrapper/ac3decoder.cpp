fa3bdcefac05726f3cd195bdc38c6b1ca2390161
Alexander Pipelka <alexander.pipelka@gmail.com>
2016-09-13 22:42:52 +0200

    [ndk] cleanup of ac3decoder

--- app/jni/msgexchange-wrapper/ac3decoder.cpp
+++ app/jni/msgexchange-wrapper/ac3decoder.cpp
@@ -1,24 +1,9 @@
 #include <stdlib.h>
-#include <string.h>
 #include "ac3decoder.h"
 #include <android/log.h>
 
 #define LOG_TAG "AC3Decoder"
 #define ALOG(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
-
-extern "C" {
-#include "mm_accel.h"
-}
-
-inline int16_t convert (int32_t i)
-{
-    if (i > 0x43c07fff)
-	return 32767;
-    else if (i < 0x43bf8000)
-	return -32768;
-    else
-	return i - 0x43c00000;
-}
 
 Ac3Decoder::Ac3Decoder(int flags) : Decoder(flags) {
 	mState = a52_init(0);
@@ -29,7 +14,7 @@ Ac3Decoder::~Ac3Decoder() {
 }
 
 int Ac3Decoder::decode(MsgPacket* p, int src_length, char* BYTE, int offset, int dst_length) {
-    uint8_t* src_buffer = (uint8_t*)p->consume(src_length);
+    uint8_t* src_buffer = p->consume(src_length);
 
 	sample_t level = 32767;
 	sample_t bias = 0;
@@ -52,7 +37,7 @@ int Ac3Decoder::decode(MsgPacket* p, int src_length, char* BYTE, int offset, int
 		return 0;
 	}
 
-	uint8_t* dst_buffer = (uint8_t*)&BYTE[offset];
+	uint16_t* dst_buffer = (uint16_t*)&BYTE[offset];
 
 	// feed frame data
 
@@ -86,10 +71,7 @@ int Ac3Decoder::decode(MsgPacket* p, int src_length, char* BYTE, int offset, int
         return 0;
     }
 
-	int16_t* buffer = (int16_t*)dst_buffer;
-
 	// process all 6 blocks
-	int blockOffset = 0;
 	int p0 = 0; // destination frame pointer
 
 	for(int i = 0; i < 6; i++) {
@@ -100,7 +82,7 @@ int Ac3Decoder::decode(MsgPacket* p, int src_length, char* BYTE, int offset, int
 			return 0;
 		}
 
-		float* sample = (float*)a52_samples(mState);
+		float* sample = a52_samples(mState);
 
 		// copy block data for each channel
 		for(int c = 0; c < mChannels; c++) {
@@ -114,7 +96,7 @@ int Ac3Decoder::decode(MsgPacket* p, int src_length, char* BYTE, int offset, int
 					ALOG("*** output buffer overrun ***");
 					break;
 				}
-				buffer[p0] = (int32_t)sample[channelOffset++];
+				dst_buffer[p0] = (int16_t)sample[channelOffset++];
 				p0 += mChannels;
 			}
 		}
