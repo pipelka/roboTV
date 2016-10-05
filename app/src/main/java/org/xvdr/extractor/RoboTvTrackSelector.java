0a3dcf20d6728c3eff4771c56381fba3d8031a06
Alexander Pipelka <alexander.pipelka@gmail.com>
2016-10-05 21:31:10 +0200

    [RoboTvTrackSelector] use DefaultTrackSelector as blue-print
    - prefer selected language
    - prefer AC3 streams

--- app/src/main/java/org/xvdr/extractor/RoboTvTrackSelector.java
+++ app/src/main/java/org/xvdr/extractor/RoboTvTrackSelector.java
@@ -3,7 +3,6 @@ package org.xvdr.extractor;
 import android.os.Handler;
 
 import com.google.android.exoplayer2.C;
-import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.RendererCapabilities;
 import com.google.android.exoplayer2.source.TrackGroup;
@@ -11,6 +10,7 @@ import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.FixedTrackSelection;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.util.MimeTypes;
 
 class RoboTvTrackSelector extends DefaultTrackSelector {
 
@@ -20,55 +20,54 @@ class RoboTvTrackSelector extends DefaultTrackSelector {
         super(eventHandler);
     }
 
-    @Override
-    protected TrackSelection[] selectTracks(RendererCapabilities[] rendererCapabilities, TrackGroupArray[] rendererTrackGroupArrays, int[][][] rendererFormatSupports) throws ExoPlaybackException {
-        TrackSelection[] rendererTrackSelections = super.selectTracks(rendererCapabilities, rendererTrackGroupArrays, rendererFormatSupports);
-
-        if (audioTrackId == null || audioTrackId.isEmpty()) {
-            return rendererTrackSelections;
-        }
-
-        for (int i = 0; i < rendererCapabilities.length; i++) {
-            if(rendererCapabilities[i].getTrackType() == C.TRACK_TYPE_AUDIO) {
-                TrackSelection selection = selectTrackById(rendererTrackGroupArrays[i], rendererFormatSupports[i], audioTrackId);
-                if(selection != null) {
-                    rendererTrackSelections[i] = selection;
-                }
-            }
-        }
-
-        return rendererTrackSelections;
+    private static boolean isSupported(int formatSupport) {
+        return (formatSupport & RendererCapabilities.FORMAT_SUPPORT_MASK) == RendererCapabilities.FORMAT_HANDLED;
     }
 
-    private TrackSelection selectTrackById(TrackGroupArray rendererTrackGroupArray, int[][] formatSupport, String audioTrackId) {
+    @Override
+    protected TrackSelection selectAudioTrack(TrackGroupArray groups, int[][] formatSupport, String preferredAudioLanguage) {
         TrackGroup selectedGroup = null;
+
         int selectedTrackIndex = 0;
+        int selectedTrackScore = 0;
 
-        // iterate track group array
-        for(int groupIndex = 0; groupIndex < rendererTrackGroupArray.length && selectedGroup == null; groupIndex++) {
-
-            TrackGroup trackGroup = rendererTrackGroupArray.get(groupIndex);
+        for(int groupIndex = 0; groupIndex < groups.length; groupIndex++) {
+            TrackGroup trackGroup = groups.get(groupIndex);
             int[] trackFormatSupport = formatSupport[groupIndex];
 
-            // iterate track group
             for(int trackIndex = 0; trackIndex < trackGroup.length; trackIndex++) {
 
-                // skip unhandled formats
-                if((trackFormatSupport[trackIndex] & RendererCapabilities.FORMAT_SUPPORT_MASK) != RendererCapabilities.FORMAT_HANDLED) {
-                    continue;
-                }
+                if(isSupported(trackFormatSupport[trackIndex])) {
+                    Format format = trackGroup.getFormat(trackIndex);
+                    boolean isDefault = (format.selectionFlags & C.SELECTION_FLAG_DEFAULT) != 0;
+                    int trackScore;
 
-                Format format = trackGroup.getFormat(trackIndex);
+                    if(format.id.equals(audioTrackId)) {
+                        trackScore = 5;
+                    }
+                    else if (format.language.equals(preferredAudioLanguage)) {
+                        if (format.sampleMimeType.equals(MimeTypes.AUDIO_AC3)) {
+                            trackScore = 4;
+                        }
+                        else {
+                            trackScore = 3;
+                        }
+                    }
+                    else if (isDefault) {
+                        trackScore = 2;
+                    }
+                    else {
+                        trackScore = 1;
+                    }
 
-                // select track with matching id
-                if(format.id.equals(audioTrackId)) {
-                    selectedGroup = trackGroup;
-                    selectedTrackIndex = trackIndex;
-                    break;
+                    if (trackScore > selectedTrackScore) {
+                        selectedGroup = trackGroup;
+                        selectedTrackIndex = trackIndex;
+                        selectedTrackScore = trackScore;
+                    }
                 }
             }
         }
-
         return selectedGroup == null ? null : new FixedTrackSelection(selectedGroup, selectedTrackIndex);
     }
 
