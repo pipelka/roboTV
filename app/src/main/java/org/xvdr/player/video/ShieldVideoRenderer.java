de652fdfc8dfaf47d5ce2ff9caaaf4e477f7b10b
Alexander Pipelka <alexander.pipelka@gmail.com>
2016-11-02 19:50:13 +0100

    [ShieldVideoRenderer] enable workaround only for 1080 and 576 resolutions

--- app/src/main/java/org/xvdr/player/video/ShieldVideoRenderer.java
+++ app/src/main/java/org/xvdr/player/video/ShieldVideoRenderer.java
@@ -1,9 +1,12 @@
 package org.xvdr.player.video;
 
 import android.content.Context;
+import android.media.MediaCodec;
+import android.media.MediaCrypto;
 import android.os.Handler;
 
 import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.drm.DrmSessionManager;
 import com.google.android.exoplayer2.drm.FrameworkMediaCrypto;
 import com.google.android.exoplayer2.mediacodec.MediaCodecSelector;
@@ -13,8 +16,9 @@ import com.google.android.exoplayer2.video.VideoRendererEventListener;
 public class ShieldVideoRenderer extends MediaCodecVideoRenderer {
 
     private long startTime = 0;
+    private boolean enabled = true;
 
-    public ShieldVideoRenderer(Context context, MediaCodecSelector mediaCodecSelector, int videoScalingMode, long allowedJoiningTimeMs, DrmSessionManager<FrameworkMediaCrypto> drmSessionManager, boolean playClearSamplesWithoutKeys, Handler eventHandler, VideoRendererEventListener eventListener, int maxDroppedFramesToNotify) {
+    ShieldVideoRenderer(Context context, MediaCodecSelector mediaCodecSelector, int videoScalingMode, long allowedJoiningTimeMs, DrmSessionManager<FrameworkMediaCrypto> drmSessionManager, boolean playClearSamplesWithoutKeys, Handler eventHandler, VideoRendererEventListener eventListener, int maxDroppedFramesToNotify) {
         super(context, mediaCodecSelector, videoScalingMode, allowedJoiningTimeMs, drmSessionManager, playClearSamplesWithoutKeys, eventHandler, eventListener, maxDroppedFramesToNotify);
     }
 
@@ -25,10 +29,16 @@ public class ShieldVideoRenderer extends MediaCodecVideoRenderer {
     }
 
     @Override
+    protected void configureCodec(MediaCodec codec, Format format, MediaCrypto crypto) {
+        super.configureCodec(codec, format, crypto);
+        enabled = (format.height == 1080 || format.height == 576);
+    }
+
+    @Override
     public void render(long positionUs, long elapsedRealtimeUs) throws ExoPlaybackException {
         long currentTime = System.currentTimeMillis();
 
-        if(currentTime - startTime > 13 * 60 * 1000) {
+        if(enabled && currentTime - startTime > 13 * 60 * 1000) {
             releaseCodec();
             startTime = currentTime;
         }
