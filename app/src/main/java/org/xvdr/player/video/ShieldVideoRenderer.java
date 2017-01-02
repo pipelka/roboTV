8696f88ad9035d88de0fc620d0d8b29e9abec4c4
Alexander Pipelka <alexander.pipelka@gmail.com>
2017-01-02 20:18:16 +0100

    [ShieldVideoRenderer] minimize delay on resetting the codec
    do the reset on the last frame of a GOP (next frame will be a key-frame).

--- app/src/main/java/org/xvdr/player/video/ShieldVideoRenderer.java
+++ app/src/main/java/org/xvdr/player/video/ShieldVideoRenderer.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 
 import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.decoder.DecoderInputBuffer;
 import com.google.android.exoplayer2.drm.DrmSessionManager;
 import com.google.android.exoplayer2.drm.FrameworkMediaCrypto;
 import com.google.android.exoplayer2.mediacodec.MediaCodecInfo;
@@ -15,10 +16,16 @@ import com.google.android.exoplayer2.mediacodec.MediaCodecUtil;
 import com.google.android.exoplayer2.video.MediaCodecVideoRenderer;
 import com.google.android.exoplayer2.video.VideoRendererEventListener;
 
-public class ShieldVideoRenderer extends MediaCodecVideoRenderer {
+class ShieldVideoRenderer extends MediaCodecVideoRenderer {
+
+    final static private long resetDuration = 13 * 60 * 1000;
 
     private long startTime;
+    private long counter = 0;
+    private long lastKeyFrame = 0;
     private boolean enabled;
+    private boolean resetCodec = false;
+    private long gopSize = 0;
 
     ShieldVideoRenderer(Context context, MediaCodecSelector mediaCodecSelector, long allowedJoiningTimeMs, DrmSessionManager<FrameworkMediaCrypto> drmSessionManager, boolean playClearSamplesWithoutKeys, Handler eventHandler, VideoRendererEventListener eventListener, int maxDroppedFramesToNotify) {
         super(context, mediaCodecSelector, allowedJoiningTimeMs, drmSessionManager, playClearSamplesWithoutKeys, eventHandler, eventListener, maxDroppedFramesToNotify);
@@ -30,17 +37,37 @@ public class ShieldVideoRenderer extends MediaCodecVideoRenderer {
 
         enabled = (format.height == 1080 || format.height == 576);
         startTime = System.currentTimeMillis();
+        resetCodec = false;
     }
 
     @Override
     public void render(long positionUs, long elapsedRealtimeUs) throws ExoPlaybackException {
-        long currentTime = System.currentTimeMillis();
-        long diffMs = currentTime - startTime;
-
-        if(enabled && diffMs > 13 * 60 * 1000) {
+        if(enabled && resetCodec) {
             releaseCodec();
         }
 
         super.render(positionUs, elapsedRealtimeUs);
     }
+
+    @Override
+    protected void onQueueInputBuffer(DecoderInputBuffer buffer) {
+        long currentTime = System.currentTimeMillis();
+        long diffMs = currentTime - startTime;
+
+        counter++;
+
+        if(buffer.isKeyFrame()) {
+            gopSize = (lastKeyFrame > 0) ? (counter - lastKeyFrame) : counter;
+            lastKeyFrame = counter;
+        }
+
+        long gopFrame = counter - lastKeyFrame;
+
+        if(enabled && gopFrame == (gopSize - 1) && diffMs > resetDuration) {
+            resetCodec = true;
+        }
+
+        super.onQueueInputBuffer(buffer);
+    }
+
 }
