80c60c7736baa1b3c0b97e9a7afbe53a4f7a65a2
Alexander Pipelka <alexander.pipelka@gmail.com>
2017-05-22 11:32:15 +0200

    [Player] revert back to DefaultLoadControl

--- app/src/main/java/org/xvdr/player/Player.java
+++ app/src/main/java/org/xvdr/player/Player.java
@@ -16,7 +16,6 @@ import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.Format;
-import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.Timeline;
@@ -107,7 +106,7 @@ public class Player implements ExoPlayer.EventListener, RoboTvExtractor.Listener
         player = ExoPlayerFactory.newSimpleInstance(
                 new RoboTvRenderersFactory(context, audioPassthrough),
                 trackSelector,
-                new RoboTvLoadControl()
+                new DefaultLoadControl()
         );
 
         player.addListener(this);
