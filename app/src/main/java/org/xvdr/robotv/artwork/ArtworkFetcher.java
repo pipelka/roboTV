822abaa74c0f0dc2b3bdf952b9efeff37bdb6688
Alexander Pipelka <alexander.pipelka@gmail.com>
2017-09-28 12:01:50 +0200

    [ArtworkFetcher] do not register artwork if it comes from the cache of our server

--- app/src/main/java/org/xvdr/robotv/artwork/ArtworkFetcher.java
+++ app/src/main/java/org/xvdr/robotv/artwork/ArtworkFetcher.java
@@ -21,7 +21,7 @@ public class ArtworkFetcher {
     public final static String TMDB_APIKEY = "958abef9265db99029a13521fddcb648";
 
     private Connection mConnection;
-    private ArtworkProvider[] mProviders;
+    private ArtworkProvider[] providers;
     private String mEpgImageTemplateUrl = "";
 
     public ArtworkFetcher(Connection connection, String language) {
@@ -30,12 +30,12 @@ public class ArtworkFetcher {
         // fetch epg images template url
         mEpgImageTemplateUrl = getEpgImageTemplateUrl();
 
-        mProviders = new ArtworkProvider[5];
-        mProviders[0] = new RoboTvProvider(connection);
-        mProviders[1] = new HttpEpgImageProvider(mEpgImageTemplateUrl);
-        mProviders[2] = new TheMovieDatabase(TMDB_APIKEY, language);
-        mProviders[3] = new TheTvDb(language);
-        mProviders[4] = new StockImageProvider();
+        providers = new ArtworkProvider[5];
+        providers[0] = new RoboTvProvider(connection);
+        providers[1] = new HttpEpgImageProvider(mEpgImageTemplateUrl);
+        providers[2] = new TheMovieDatabase(TMDB_APIKEY, language);
+        providers[3] = new TheTvDb(language);
+        providers[4] = new StockImageProvider();
     }
 
     public boolean fetchForEvent(Event event) throws IOException {
@@ -44,20 +44,24 @@ public class ArtworkFetcher {
         }
 
         // sanity check
-        if(TextUtils.isEmpty(event.getTitle()) || mProviders == null) {
+        if(TextUtils.isEmpty(event.getTitle()) || providers == null) {
             return false;
         }
 
         if(mEpgImageTemplateUrl.isEmpty()) {
             mEpgImageTemplateUrl = getEpgImageTemplateUrl();
-            mProviders[1] = new HttpEpgImageProvider(mEpgImageTemplateUrl);
+            providers[1] = new HttpEpgImageProvider(mEpgImageTemplateUrl);
         }
 
         ArtworkHolder o = null;
 
         // try all providers
-        for (ArtworkProvider provider : mProviders) {
+        boolean registerOnServer = true;
+
+        for (ArtworkProvider provider : providers) {
             if ((o = provider.search(event)) != null) {
+                // skip registering the artwork if it comes from our server cache
+                registerOnServer = (provider != providers[0]);
                 break;
             }
         }
@@ -68,19 +72,21 @@ public class ArtworkFetcher {
         }
 
         // register artwork on server
-        Packet req = mConnection.CreatePacket(Connection.XVDR_ARTWORK_SET);
-        req.putString(event.getTitle());
-        req.putU32(event.getContentId());
-        req.putString(o.getPosterUrl().equals("x") ? "" : o.getPosterUrl());
-        req.putString(o.getBackgroundUrl().equals("x") ? "" : o.getBackgroundUrl());
-        req.putU32(0);
+        if(registerOnServer) {
+            Packet req = mConnection.CreatePacket(Connection.XVDR_ARTWORK_SET);
+            req.putString(event.getTitle());
+            req.putU32(event.getContentId());
+            req.putString(o.getPosterUrl().equals("x") ? "" : o.getPosterUrl());
+            req.putString(o.getBackgroundUrl().equals("x") ? "" : o.getBackgroundUrl());
+            req.putU32(0);
 
-        // update EPG entry
-        req.putU32(event.getChannelUid());
-        req.putU32(event.getEventId());
+            // update EPG entry
+            req.putU32(event.getChannelUid());
+            req.putU32(event.getEventId());
 
-        if(mConnection.transmitMessage(req) == null) {
-            Log.d(TAG, "failed to register artwork for '" + event.getTitle() + "' in cache");
+            if (mConnection.transmitMessage(req) == null) {
+                Log.d(TAG, "failed to register artwork for '" + event.getTitle() + "' in cache");
+            }
         }
 
         event.setPosterUrl(o.getPosterUrl());
