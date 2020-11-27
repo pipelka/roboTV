afcc760f453b26ed82be41976de56f45fe28501b
Alexander Pipelka <alexander.pipelka@gmail.com>
2020-11-27 13:13:23 +0100

    [ArtworkUtils] actually send message in setMovieArtwork()

--- robotv-client/src/main/java/org/robotv/client/artwork/ArtworkUtils.java
+++ robotv-client/src/main/java/org/robotv/client/artwork/ArtworkUtils.java
@@ -1,10 +1,14 @@
 package org.robotv.client.artwork;
 
+import android.util.Log;
+
 import org.robotv.msgexchange.Packet;
 import org.robotv.client.model.Movie;
 import org.robotv.client.Connection;
 
 public class ArtworkUtils {
+
+    final private static String TAG = ArtworkUtils.class.getName();
 
     public static void setMovieArtwork(Connection connection, Movie movie, ArtworkHolder holder) {
         movie.setPosterUrl(holder.getPosterUrl());
@@ -20,6 +24,12 @@ public class ArtworkUtils {
         p.putString(movie.getPosterUrl());
         p.putString(movie.getBackgroundUrl());
         p.putU32(0);
+
+        Packet r = connection.transmitMessage(p);
+
+        if(r == null) {
+            Log.d(TAG, "failed to update move artwork");
+        }
     }
 
 }
