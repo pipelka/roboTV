782129f2346c6777edf7207b799b6b65fb8e1726
Alexander Pipelka <alexander.pipelka@gmail.com>
2020-12-21 21:27:53 +0100

    [robotv-atv] LatestCardPresenter: added long-click handler

--- robotv-atv/src/main/java/org/robotv/recordings/presenter/LatestCardPresenter.java
+++ robotv-atv/src/main/java/org/robotv/recordings/presenter/LatestCardPresenter.java
@@ -1,11 +1,12 @@
 package org.robotv.recordings.presenter;
 
 import android.content.Context;
-import androidx.leanback.widget.ImageCardView;
+
 import androidx.leanback.widget.Presenter;
 
 import org.robotv.client.Connection;
 import org.robotv.client.model.Movie;
+import org.robotv.recordings.model.MovieImageCardView;
 import org.robotv.recordings.util.Utils;
 import org.robotv.robotv.R;
 
@@ -19,7 +20,7 @@ public class LatestCardPresenter extends MoviePresenter {
     public void onBindViewHolder(Presenter.ViewHolder viewHolder, Object item) {
         Movie movie = (Movie) item;
         MoviePresenter.ViewHolder vh = (ViewHolder) viewHolder;
-        ImageCardView cardView = vh.getCardView();
+        MovieImageCardView cardView = vh.getCardView();
         Context context = cardView.getContext();
 
         String contextText = movie.getDate() + " " + movie.getTime();
@@ -29,7 +30,20 @@ public class LatestCardPresenter extends MoviePresenter {
         cardView.setMainImageDimensions(MoviePresenter.WIDTH, MoviePresenter.HEIGHT);
         cardView.setInfoAreaBackgroundColor(Utils.getColor(context, R.color.primary_color));
 
+        cardView.setOnLongClickListener(v -> {
+            if(listener == null) {
+                return false;
+            }
+
+            return listener.onLongClick(movie);
+        });
+
         vh.update(movie, connection, context);
     }
 
+    @Override
+    public void onUnbindViewHolder(Presenter.ViewHolder viewHolder) {
+        ViewHolder vh = (ViewHolder) viewHolder;
+        vh.getCardView().setOnLongClickListener(null);
+    }
 }
