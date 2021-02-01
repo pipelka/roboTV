8e243a23ba0940c9e4530c0d82ec4da2df35248d
Alexander Pipelka <alexander.pipelka@gmail.com>
2021-02-01 14:00:09 +0100

    [robotv-atv] BootCompletedReceiver: catch exceptions

--- robotv-atv/src/main/java/org/robotv/system/service/BootCompletedReceiver.java
+++ robotv-atv/src/main/java/org/robotv/system/service/BootCompletedReceiver.java
@@ -1,17 +1,26 @@
 package org.robotv.system.service;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import org.robotv.dataservice.DataService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
+    static final String TAG = BootCompletedReceiver.class.getName();
+
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent serviceIntent = new Intent(context, DataService.class);
-        context.startService(serviceIntent);
+        try {
+            context.startService(serviceIntent);
+        }
+        catch(IllegalStateException | SecurityException e) {
+            Log.d(TAG, "Failed to start service on boot - not permitted");
+        }
     }
-
 }
